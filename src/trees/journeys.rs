0878a35ec8a6df20f3b6760defb33e02a9618ddb
Robin McAndrew <robin.mcandrew@agiledigital.com.au>
2025-11-07 12:49:59 +1100

    Endpoint to pull scripts for the journey

--- src/trees/journeys.rs
+++ src/trees/journeys.rs
@@ -6,7 +6,6 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
 use crate::NodeOutcomeEdge;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
