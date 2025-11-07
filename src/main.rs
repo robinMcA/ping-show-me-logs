fa39174cead36dd090259c2a7672c88360757638
Robin McAndrew <robin.mcandrew@agiledigital.com.au>
2025-11-07 19:29:07 +1100

    start the move of logs into mod

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 use crate::errors::ShowMeErrors;
-use crate::token::{Token, get_usable_token};
-use crate::trees::journeys::{AuthenticationTreeList, ReactFlowEdge, ReactFlowNode, Tree};
+use crate::ping_logs::logs::get_logs;
+use crate::token::{get_usable_token, Token};
+use crate::trees::journeys::{AuthenticationTreeList, ReactFlowEdge, ReactFlowNode};
 use crate::trees::nodes::{NodeConfig, NodeData};
 use actix_web::http::header::ContentType;
 use actix_web::rt::time::sleep;
 use actix_web::web::Query;
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, mime, post, rt, web};
+use actix_web::{get, mime, post, rt, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_ws::{AggregatedMessage, Session};
 use chrono::{DateTime, Utc};
 use futures_util::{StreamExt as _, TryFutureExt};
+use ping_logs::logs::{Level, Logs};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::ops::Deref;
 use std::path::PathBuf;
-use std::sync::mpsc::Receiver;
-use std::sync::{Arc, LockResult, Mutex, mpsc};
+use std::sync::Mutex;
 use std::time::Duration;
 
 mod errors;
 mod token;
 mod trees;
+mod ping_logs;
 
 struct AppMutState {
   transaction_id: Mutex<String>,
@@ -33,114 +35,6 @@ struct AppMutState {
   key: String,
   log: String,
   ws: Mutex<Vec<Session>>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
-#[serde(rename_all = "UPPERCASE")]
-enum Level {
-  Debug,
-  Warning,
-  Warn,
-  Info,
-  Error,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-struct NodeOutcomeInfo {
-  node_extra_logging: Option<serde_json::Map<String, serde_json::Value>>,
-  node_id: String,
-  node_outcome: String,
-  display_name: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-struct NodeOutcome {
-  info: NodeOutcomeInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-struct PingPayload {
-  context: Option<String>,
-  level: Level,
-  entries: Option<Vec<NodeOutcome>>,
-  logger: Option<String>,
-  message: Option<String>,
-  transaction_id: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct ResultingLog {
-  payload: PingPayload,
-  timestamp: DateTime<Utc>,
-  #[serde(rename = "type")]
-  data_type: String,
-  source: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-struct Logs {
-  result: Vec<ResultingLog>,
-  paged_result_cooke: Option<String>,
-  total_paged_results_policy: String,
-  total_paged_results: i16,
-  remaining_paged_results: i16,
-}
-
-impl Logs {
-  pub fn filter_logs(self, level: Level) -> Logs {
-    let result = self
-      .result
-      .iter()
-      .filter(|t| t.payload.level == level)
-      .cloned()
-      .collect::<Vec<_>>()
-      .clone();
-    Logs { result, ..self }
-  }
-}
-
-async fn get_logs(
-  client: &Client,
-  transaction_id: &str,
-  query_filter: Option<&str>,
-) -> Result<Logs, ShowMeErrors> {
-  let params = [
-    ("source", "am-everything,idm-everything"),
-    ("transactionId", transaction_id),
-    (
-      "_queryFilter",
-      match query_filter {
-        Some(filter_string) => filter_string,
-        None => "",
-      },
-    ),
-  ];
-
-  let url = std::env::var("SANDBOX")?;
-  let key = std::env::var("PING_KEY")?;
-  let sec = std::env::var("PING_SEC")?;
-  match client
-    .get(url)
-    .query(&params)
-    .header("x-api-key", key)
-    .header("x-api-secret", sec)
-    .send()
-    .await
-  {
-    Ok(res) => match res.bytes().await {
-      Ok(bty) => Ok(serde_json::from_slice(&bty)?),
-      Err(e) => {
-        Err(ShowMeErrors::PingApiError(/* reqwest::Error */ e))
-      }
-    },
-    Err(e) => {
-      Err(ShowMeErrors::PingApiError(/* reqwest::Error */ e))
-    }
-  }
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -186,7 +80,7 @@ async fn logs(
 
   let query_filter = defined_filters.clone().join(" or ");
 
-  match get_logs(&Client::new(), &id, Some(query_filter.as_str())).await {
+  match ping_logs::logs::get_logs(&Client::new(), &id, Some(query_filter.as_str())).await {
     Ok(ll) => Ok(match query.filters.clone() {
       None => web::Json(ll),
       Some(filter) => match filter {
